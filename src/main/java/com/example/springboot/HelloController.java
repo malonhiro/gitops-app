92e66033403f27edad0312383dc4105bb24e6a16
kato.takahiro <kato.takahiro@mind.co.jp>
2022-10-19 07:52:48 +0000

    Revert "app update test"
    This reverts commit 83723ce7b68212c4160b15e4fda1423536c5d1ad.

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! 20221019-01";
+		return "Greetings from Spring Boot + Tanzu!";
 	}
 
 }
