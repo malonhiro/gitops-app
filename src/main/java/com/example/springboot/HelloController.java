ea05ad27a97e9c7e06c6fda8726bc049b9bf013f
kato.takahiro <kato.takahiro@mind.co.jp>
2022-10-20 23:26:41 +0000

    app update test

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! 20221019-99";
+		return "Greetings from Spring Boot + Tanzu! 20221021-01";
 	}
 
 }
